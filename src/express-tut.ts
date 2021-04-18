06dc13d1e363395535a42754c7f2926eab7711aa
Mirco Kraenz <kraenzmirco@gmail.com>
2021-04-18 19:36:12 +0200

    ADD #1 pkmn rest-ish API not a real RESTful api, but close
    Co-authored-by: knoffi <knoffi@users.noreply.github.com>

---
+++ src/express-tut.ts
@@ -0,0 +1,76 @@
+import express from 'express';
+import { Pokemon } from '.';
+
+const port = 3000;
+
+const app = express();
+
+app.use(express.json());
+
+let pokemons: Pokemon[] = [];
+let id = 1;
+
+// browser  <-> app <-> database
+//              app <-> database
+// browser  <-> app
+
+app.get('/', function (req, res) {
+  console.log('Got a GET request for the homepage');
+  res.send(pokemons);
+});
+
+const isPokemon = (data: any) => {
+  const hasName =
+    typeof data.name === 'string' && legalDepartmentApproves(data.name);
+  return hasName;
+};
+
+// https://en.wikipedia.org/wiki/Representational_state_transfer REST API
+app.post('/', function (req, res) {
+  const body: Omit<Pokemon, 'id'> = req.body;
+  const newPokemon: Pokemon = { ...body, id: id };
+  if (isPokemon(newPokemon)) {
+    pokemons.push(newPokemon);
+    id++;
+  } else {
+    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses
+    res.status(400).send('Pokemon name is not working!');
+  }
+  console.log('Got a POST request for the homepage');
+  res.send(pokemons.map((pokemon) => pokemon.name));
+});
+
+// DELETE localhost:3000/6
+// -> req.params.id = 6
+app.delete('/:id', function (req, res) {
+  pokemons = pokemons.filter(
+    (pokemon) => pokemon.id !== parseInt(req.params.id)
+  );
+  console.log('Got a DELETE request for /del_user');
+  res.send('Hello DELETE');
+});
+
+app.get('/:id', function (req, res) {
+  console.log('Got a GET request for /list_user');
+  res.send(
+    pokemons.find((pokemon) => pokemon.id === parseInt(req.params.id)) || {
+      name: 'Missingno.',
+    }
+  );
+});
+
+app.listen(port, function () {
+  const host = 'localhost';
+
+  console.log(`Example app listening at http://${host}:${port}`);
+});
+
+function legalDepartmentApproves(name: string) {
+  const illegalNames = [''];
+  return !illegalNames.includes(name);
+}
+// http://localhost:3000/
+
+// Browser -> open URL -> send GET localhost:3000
+// -> server receives GET request to localhost:3000
+// ->
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
 import { Collection, MongoClient } from 'mongodb';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
-interface Pokemon {
+const setupMongoDbMemoryServer = async () => {
+  const mongod = new MongoMemoryServer();
+
+  const uri = await mongod.getUri();
+  const port = await mongod.getPort();
+  const dbPath = await mongod.getDbPath();
+  const dbName = await mongod.getDbName();
+  return { uri, port, dbPath, dbName };
+};
+
+export interface Pokemon {
+  id: number;
   name: string;
   level: number;
   type: 'Water' | 'Fire' | 'Plant' | 'Normal';
@@ -14,12 +26,14 @@ interface Pokemon {
 
 type PokemonCollection = Collection<Pokemon>;
 
-const uri = 'mongodb://localhost:27017';
-
-const client = new MongoClient(uri, { useUnifiedTopology: true });
+// const uri = 'mongodb://localhost:27017';
 
 async function run() {
+  let client: MongoClient;
   try {
+    const { uri } = await setupMongoDbMemoryServer();
+
+    client = new MongoClient(uri, { useUnifiedTopology: true });
     await client.connect();
     const database = await client.db('pokemon');
     const pokemons = database.collection<Pokemon>('pokemons');
@@ -29,12 +43,13 @@ async function run() {
       hp: 50,
       type: 'Plant',
       attacks: [{ name: 'tackle', damage: 5, type: 'Normal' }],
+      id: 123,
     });
     const pkms = await findAll(pokemons);
     console.dir(pkms, { depth: Infinity });
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    await client!.close();
   }
 }
 
